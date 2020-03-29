e3d144893d28054d1e1742feb014f8eda087f83e
pranav8596 <pige786123@gmail.com>
2020-03-29 20:19:02 +0530

    ADDED:Singlet combination of coin & percentage stored in Dictionary

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,54 @@
 #!/bin/bash -x
 
-isHead=1
-randomCheck=$((RANDOM%2))
-if [ $randomCheck -eq $isHead ]
-then
-	echo "HEAD"
-else
-	echo "TAIL"
-fi
+#Constants
+SINGLET=1
+
+#Declaration of a Dictionary
+declare -A flipCoinCombinations
+
+#Flip a coin to display Head or Tail
+function flipACoin() {
+	isHead=1
+	randomCheck=$((RANDOM%2))
+	if [ $randomCheck -eq $isHead ]
+	then
+		coinSide+=H
+	else
+		coinSide+=T
+	fi
+}
+
+#To get all the combinations of the coins
+function coinCombinations() {
+	numberOfCoins=$1
+	for ((i=1; i<=$numberOfFlips; i++))
+	do
+		coinSide=""
+		for ((j=1; j<=$numberOfCoins; j++))
+		do
+			flipACoin
+		done
+		storeInDictionary $coinSide $2
+	done
+	echo "Keys:       ${!flipCoinCombinations[@]}"
+	echo "Count:      ${flipCoinCombinations[@]}"
+	calculatePercentage
+}
+
+#To store the combinations in a Dictionary
+function storeInDictionary() {
+	flipCoinCombinations[$coinSide]=$((${flipCoinCombinations[$coinSide]} + 1))
+}
+
+#To calculate percentages of the combinations
+function calculatePercentage() {
+	for i in ${!flipCoinCombinations[@]}
+	do
+		flipCoinCombinations[$i]=$(($((${flipCoinCombinations[$i]}*100))/$numberOfFlips))
+	done
+	echo "Percentage: ${flipCoinCombinations[@]}"
+}
+
+#Main
+read -p "Enter the number of times you want to flip a coin: " numberOfFlips
+coinCombinations $SINGLET flipCoinCombinations
